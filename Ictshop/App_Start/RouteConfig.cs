bcd237b7e3122feb8712720f22fb4da942bb1adf
= <=nguyenthehunggg321@gmail.com>
2024-07-05 16:33:44 +0700

    dienthoai project

---
+++ Ictshop/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ictshop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+               // new[] { "Ictshop.Controllers" }
+               namespaces: new[] { "Ictshop.Controllers" }
+
+            );
+
+           // routes.MapRoute(
+           //    name: "Demo",
+           //    url: "{controller}/{action}",
+           //    defaults: new { controller = "SanPham", action = "demo" }
+
+           //);
+        }
+    }
+}
